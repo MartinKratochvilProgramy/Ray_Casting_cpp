0c5deb7f032c884ee90f9baceef0bb09d6225ad5
martin.kratochvil <martvil96@gmail.com>
2022-01-17 07:11:05 +0100

    changed comments in main

--- rayTracer/main.cpp
+++ rayTracer/main.cpp
@@ -1,13 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include "Grid.h"
 
-//Creates 800x800 grid with 50x50 divisions
-//Press F to show rays, R to hide rays
-//left click creates blocks on grid
-//A and D rotates rays
 
 int main()
 {
+
     Grid grid(800, 800, 50, 50, 1); //WIDTH / HEIGHT / #of divisions in x-dir / #of divisions in y-dir / grid border
 
     grid.run();
