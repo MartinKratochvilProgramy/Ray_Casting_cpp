8d26c04f3e6acd96233b05e072d1c57bbbbd9a10
MartinKratochvilProgramy <martvil96@gmail.com>
2022-02-01 07:02:37 +0100

    edited comment in main

--- rayTracer/main.cpp
+++ rayTracer/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 
-    Grid grid(800, 800, 50, 50, 1); //WIDTH / HEIGHT / #of divisions in x-dir / #of divisions in y-dir / grid border_
+    Grid grid(800, 800, 50, 50, 1); //WIDTH / HEIGHT / #of divisions in x-dir / #of divisions in y-dir / grid border
 
     grid.run();
 
